15988e048821e3f3a68e270194c24aa9a04b60cc
eduardcadar <eduard.cadar@stud.ubbcluj.ro>
2023-03-14 16:19:53 +0200

    changed code to fix identified coding defects

--- src/main/java/pizzashop/controller/KitchenGUIController.java
+++ src/main/java/pizzashop/controller/KitchenGUIController.java
@@ -6,6 +6,7 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+
 import java.util.Calendar;
 
 public class KitchenGUIController {
@@ -16,13 +17,13 @@ public class KitchenGUIController {
     @FXML
     public Button ready;
 
-    public static  ObservableList<String> order = FXCollections.observableArrayList();
+    public static ObservableList<String> order = FXCollections.observableArrayList();
     private Object selectedOrder;
     private Calendar now = Calendar.getInstance();
     private String extractedTableNumberString = new String();
     private int extractedTableNumberInteger;
     //thread for adding data to kitchenOrderList
-    public  Thread addOrders = new Thread(new Runnable() {
+    public Thread addOrders = new Thread(new Runnable() {
         @Override
         public void run() {
             while (true) {
@@ -30,11 +31,11 @@ public class KitchenGUIController {
                     @Override
                     public void run() {
                         kitchenOrdersList.setItems(order);
-                        }
+                    }
                 });
                 try {
                     Thread.sleep(100);
-                  } catch (InterruptedException ex) {
+                } catch (InterruptedException ex) {
                     break;
                 }
             }
@@ -48,10 +49,12 @@ public class KitchenGUIController {
         //Controller for Cook Button
         cook.setOnAction(event -> {
             selectedOrder = kitchenOrdersList.getSelectionModel().getSelectedItem();
+            if (selectedOrder == null)
+                return;
             kitchenOrdersList.getItems().remove(selectedOrder);
             kitchenOrdersList.getItems().add(selectedOrder.toString()
-                     .concat(" Cooking started at: ").toUpperCase()
-                     .concat(now.get(Calendar.HOUR)+":"+now.get(Calendar.MINUTE)));
+                    .concat(" Cooking started at: ").toUpperCase()
+                    .concat(now.get(Calendar.HOUR) + ":" + now.get(Calendar.MINUTE)));
         });
         //Controller for Ready Button
         ready.setOnAction(event -> {
@@ -60,7 +63,7 @@ public class KitchenGUIController {
             extractedTableNumberString = selectedOrder.toString().subSequence(5, 6).toString();
             extractedTableNumberInteger = Integer.valueOf(extractedTableNumberString);
             System.out.println("--------------------------");
-            System.out.println("Table " + extractedTableNumberInteger +" ready at: " + now.get(Calendar.HOUR)+":"+now.get(Calendar.MINUTE));
+            System.out.println("Table " + extractedTableNumberInteger + " ready at: " + now.get(Calendar.HOUR) + ":" + now.get(Calendar.MINUTE));
             System.out.println("--------------------------");
         });
     }
